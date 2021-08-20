a89468756379752e9ff361a7b5c235d523818a0e
Ali Enes Arslan <88050660+Alenarss@users.noreply.github.com>
2021-08-20 15:43:41 +0300

    Update test.java

--- src/test.java
+++ src/test.java
@@ -1,6 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Git'ten merhaba");
-
+        System.out.println("GitHub'tan merhaba");
+            
+        
     }
 }
